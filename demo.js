093f735010fcd723a191f189b40aae44abf10081
Hunter Cote <hcote12@gmail.com>
2019-04-03 15:03:58 -0700

    starting over - angluar project started, node project started

--- demo.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    console.log(req);
-    console.log(res);
-    console.log(document);
-    res.write('Hello World!');
-    res.end();
-}).listen(8080);
