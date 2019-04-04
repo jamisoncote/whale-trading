3b697327318867c358b5de7f2ebde401409c72b6
Hunter Cote <hcote12@gmail.com>
2019-04-03 17:28:57 -0700

    cleaning up db connection file

--- backend/db_connection.js
+++ backend/db_connection.js
@@ -3,17 +3,9 @@ var conString = "postgres://postgres:5432@localhost/postgres";
 
 var client = new pg.Client(conString);
 
-// var query = client.query('SELECT NOW() AS "theTime"');
-// query.on('row', function(row) {
-//     console.log(row);
-// });
-// query.on('end', function() {
-//     client.end();
-// });
-
 client.connect(function(err) {
     if(err) {
-      return console.error('could not connect to postgres', err);
+       return console.error('could not connect to postgres', err);
     }
     client.query('SELECT NOW() AS "theTime"', function(err, result) {
       if(err) {
